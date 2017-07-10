0b8af98de984a4eb2686ef8d29937ff39706f10a
Alexander <alexander.mykulych@gmail.com>
2017-07-10 08:52:45 +0300

    init http IfF point lib

---
+++ InterflowFramework/App1/Integrator.cs
@@ -0,0 +1,29 @@
+﻿using InterflowFramework.Core.Channel.InputPoint;
+using InterflowFramework.Core.Channel.InputPoint.Model;
+using InterflowFramework.Core.Factory.Channel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1 {
+	public class Integrator {
+		public MessageChannelCreator Channel;
+		public IInputPoint InputPoint;
+		public IOutputPoint OutputPoint;
+		public Integrator() {
+			InputPoint = new SimpleInputPoint();
+			OutputPoint = new 
+			Channel = new MessageChannelCreator()
+				.In(InputPoint)
+				.Out();
+		}
+		public bool Integrate(Contact contact) {
+			
+
+
+			return true;
+		}
+	}
+}
